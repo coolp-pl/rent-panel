2ef580092123ee4a2375cb6adc884255f9de33dc
Michal Zmuda <zmu.michal@gmail.com>
2015-03-02 13:47:18 +0100

    navbar refinements & current logged added to view scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static#home'
+  get 'static/home'
+
   devise_for :admins
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
