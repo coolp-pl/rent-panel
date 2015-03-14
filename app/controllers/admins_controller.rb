1e542822fb11c759585641c7e4cb05a1b706c812
Michal Zmuda <zmu.michal@gmail.com>
2015-03-14 15:40:00 +0100

    admins crud

---
+++ app/controllers/admins_controller.rb
@@ -0,0 +1,72 @@
+class AdminsController < ApplicationController
+  before_action :set_admin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_admin!
+
+  # GET /admins
+  # GET /admins.json
+  def index
+    @admins = Admin.all
+  end
+
+  # GET /admins/1
+  # GET /admins/1.json
+  def show
+    render :edit
+  end
+
+  # GET /admins/new
+  def new
+    @admin = Admin.new
+  end
+
+  # GET /admins/1/edit
+  def edit
+  end
+
+  # POST /admins
+  # POST /admins.json
+  def create
+    @admin = Admin.new(admin_params)
+
+    respond_to do |format|
+      if @admin.save
+        format.html { redirect_to @admin, notice: 'Admin was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  # PATCH/PUT /admins/1
+  # PATCH/PUT /admins/1.json
+  def update
+    respond_to do |format|
+      if @admin.update(admin_params)
+        format.html { redirect_to @admin, notice: 'Admin was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  # DELETE /admins/1
+  # DELETE /admins/1.json
+  def destroy
+    @admin.destroy
+    respond_to do |format|
+      format.html { redirect_to admins_url, notice: 'Admin was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end
+
+# Never trust parameters from the scary internet, only allow the white list through.
+    def admin_params
+      params.require(:admin).permit(:email, :password, :password_confirmation, :first_name, :last_name)
+    end
+end
