1e1f8d138706bc0cc08914cbc6fa7264d086588f
Michał Żmuda <zmu.michal@gmail.com>
2015-07-19 16:02:29 +0200

    minor problems with translations fixed

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,8 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    last_name + ' ' + first_name
+  end
 end
